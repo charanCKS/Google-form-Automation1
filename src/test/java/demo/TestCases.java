60574f0110ec258d16abe4033fe584366cbdeb3e
Mahesh <kmaheshmec@gmail.com>
2024-06-15 21:36:57 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,34 +1,32 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    Wrappers wrappers;
 
-    /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
-     */
-
-     
-    /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
-     */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -42,18 +40,113 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
-
+        wrappers = new Wrappers(driver);
         driver.manage().window().maximize();
     }
 
     @AfterTest
-    public void endTest()
-    {
-        driver.close();
-        driver.quit();
+    public void endTest() {
+        if (driver != null) {
+            driver.close();
+            driver.quit();
+        }
+    }
 
+    @Test
+    public void testCase01() throws InterruptedException {
+        System.out.println("Start Test case: testCase01");
+        driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        
+
+        Thread.sleep(1000);
+
+        By nameLocator = By.xpath("//div[@class='rFrNMe k3kHxc RdH0ib yqQS1 zKHdkd']/div[1]/div[1]/div[1]/input");
+        wrappers.sendKeys(nameLocator, "Crio Learner");
+
+        By practicingAutomationLocator = By.xpath("//textarea[@class='KHxj8b tL9Q4c']");
+        Long date = 1715515721L;
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        String convertedDate = sdf.format(new java.util.Date(date * 1000));
+        wrappers.sendKeys(practicingAutomationLocator, "I want to be the best QA Engineer! " + convertedDate);
+
+        wrappers.scroll(0, 500);
+
+        wrappers.click(By.id("i22"));
+        wrappers.click(By.id("i30"));
+        wrappers.click(By.id("i33"));
+        wrappers.click(By.id("i36"));
+        wrappers.click(By.id("i39"));
+
+        Thread.sleep(2000);
+
+        By addressedLocator = By.xpath("//div[@class='MocG8c HZ3kWc mhLiyf LMgvRb KKjvXb DEh1R']");
+        wrappers.click(addressedLocator);
+        Thread.sleep(2000);
+        wrappers.click(By.xpath("//div[@role='option']//span[@class='vRMGwf oJeWuf'][normalize-space()='Mr']"));
+
+        Thread.sleep(2000);
+
+        wrappers.scroll(0, 900);
+        Thread.sleep(2000);
+
+
+        Actions actions = new Actions(driver);
+
+        long timtam 
+        = 1715515721L;
+          
+        
+        long mlisec 
+        = timtam * 1000;
+        
+    
+        long neTimta
+         = mlisec-(7*24*60*
+        60*1000);
+        
+        Date newDte = new Date(neTimta);
+        
+        SimpleDateFormat sd   =        new
+         SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        String convertDate 
+        = sd.format(newDte);
+
+        actions.moveToElement(driver.findElement(By.xpath("//div[@class='rFrNMe yqQS1 hatWr zKHdkd']")))
+        
+        .click().sendKeys("2024").sendKeys(Keys.ARROW_LEFT).sendKeys("06").sendKeys(Keys.ARROW_LEFT).sendKeys(Keys.ARROW_LEFT).sendKeys(convertDate).click().perform();
+        
+        driver.findElement(By.xpath("//div[@class='A6uyJd']")).click();
+        Thread.sleep(2000);
+
+
+
+
+       
+
+        wrappers.click(By.xpath("//div[@jscontroller='OZjhxc']"));
+
+        LocalTime currentTime = LocalTime.now();
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        String formattedCurrentTime = currentTime.format(timeFormatter);
+
+        actions.moveToElement(wrappers.findElement(By.xpath("//input[@aria-label='Hour']")))
+               .click().sendKeys(formattedCurrentTime.split(":")[0])
+               .sendKeys(Keys.TAB).sendKeys(formattedCurrentTime.split(":")[1])
+               .sendKeys(Keys.TAB).perform();
+
+        Thread.sleep(2000);
+
+        By submitButtonLocator = By.xpath("//span[contains(text(),'Submit')]");
+        WebElement submitButton = wrappers.findElement(submitButtonLocator);
+        System.out.println("Submit button text: " + submitButton.getText());
+
+        submitButton.click();
+        Thread.sleep(2000);
+
+        System.out.println("Thanks for your response, Automation Wizard!");
+        System.out.println("End Test case: testCase01");
     }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -6,6 +6,8 @@ import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,6 +16,28 @@ import java.time.Duration;
 
 public class Wrappers {
     /*
-     * Write your selenium wrappers here
+     * Writing selenium wrappers here
      */
+    WebDriver driver;
+
+    public Wrappers(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    public void sendKeys(By locator, String text) {
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    public void scroll(int x, int y) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(" + x + ", " + y + ")");
+    }
+
+    public WebElement findElement(By locator) {
+        return driver.findElement(locator);
+    }
 }
